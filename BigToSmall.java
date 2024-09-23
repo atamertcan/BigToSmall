38c54392fb8e8dcf7117c871cc4d4bb970812caa
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-23 23:56:57 +0300

    BigToSmall.java

---
+++ BigToSmall.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] firstArray = randomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+
+        Arrays.sort(firstArray);
+
+        int temp;
+        boolean flag = true;
+
+        for(int i = 0; i < firstArray.length; i++){
+
+            for(int j = 0; j < firstArray.length - 1; j++){
+
+                if(firstArray[j] < firstArray[j + 1]){
+                    temp = firstArray[j];
+                    firstArray[j] = firstArray[j + 1];
+                    firstArray[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(firstArray));
